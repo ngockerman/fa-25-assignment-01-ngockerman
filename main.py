3dc2eca8934e7f2071eb6425363793a2bb10a162
ngockerman <ngockerman@tulane.edu>
2025-09-15 12:33:47 -0500

    Update main.py
    attempt 1

--- main.py
+++ main.py
@@ -6,22 +6,22 @@ See assignment-01.pdf for details.
 
 def foo(x):
     ### TODO
-    if x <= 1:
+    if x <= 1: # handles base cases of x = 0 or 1
         return x
-    ra = foo(x - 1)
-    rb = foo(x - 2)
-    return ra + rb
+    minus_one = foo(x - 1)  # recursively calls x-1
+    minus_two = foo(x - 2)  # recursively calls x-2
+    return minus_one + minus_two   # returns summation of 2 previous terms to find correct fibonacci number
 
 def longest_run(mylist, key):
     ### TODO
-    best = 0
-    cur = 0
-    for v in mylist:
-        if v == key:
-            cur += 1
-            if cur > best:
-                best = cur
-        else:
+    best = 0  # initializes variable to hold best run so far
+    cur = 0   # initializes variable to hold current run
+    for v in mylist:  # iterates through list
+        if v == key:  # if reaches an element that matches the inputted key
+            cur += 1  # extends the current run by 1
+            if cur > best:  # if the current run is larger than previous best run, updates best run to current
+                best = cur 
+        else:  # if not, reset current run to 0 and move to next
             cur = 0
     return best
 
@@ -51,28 +51,38 @@ def to_value(v):
         
 def longest_run_recursive(mylist, key):
     ### TODO
-    n = len(mylist)
-    if n == 0:
+    n = len(mylist)   # initializes variable to hold the lenght of the list
+    
+    if n == 0:  # handles base case that the list is empty
         return Result(0, 0, 0, True)
-    if n == 1:
+        
+    if n == 1:  # handles base case that the lsit has only one element
         if mylist[0] == key:
+            # if the single element does match the key, there is a run length of 1
             return Result(1, 1, 1, True)
         else:
+            # if the single element does not match the key, there is no run length
             return Result(0, 0, 0, False)
 
+    # handles recursive cases by spliting the list into two halves
     mid = n // 2
     left  = longest_run_recursive(mylist[:mid], key)
     right = longest_run_recursive(mylist[mid:], key)
 
+    # checks if both halves are uniform
     is_entire = left.is_entire_range and right.is_entire_range
 
+    # checks left and right runs by adding size, possibly extended if uniform
     left_size  = left.left_size  + (right.left_size  if left.is_entire_range  else 0)
     right_size = right.right_size + (left.right_size if right.is_entire_range else 0)
 
+    # combines runs of left half of right and right half of left
     cross = left.right_size + right.left_size
 
+    # finds the maximum runtime of left, right, and cross
     longest = max(left.longest_size, right.longest_size, cross)
 
+    # returns a combined result
     return Result(left_size, right_size, longest, is_entire)
 
 
